c8a83c2556a71f7be6c0c3a5842659c31aee02b6
kuduka <kuduka@gmail.com>
2018-09-19 17:48:04 +0200

    Initial commit

---
+++ app/src/main/java/com/soc/uoc/pqtm/mybooks/models/BookItem.java
@@ -0,0 +1,15 @@
+package com.soc.uoc.pqtm.mybooks.models;
+
+import java.util.Date;
+
+public class BookItem {
+
+    Integer id;
+    String title;
+    String author;
+    Date dpublish;
+    String desc;
+    String imgurl;
+
+    
+}
