e269ad9f371c70bf3b133bb7e70ca9b98426e10c
kuduka <kuduka@gmail.com>
2018-10-27 19:05:48 +0200

    [ADD] Exer3.1

---
+++ app/src/main/java/com/soc/uoc/pqtm/mybooks/MyFirebaseMessagingService.java
@@ -0,0 +1,80 @@
+package com.soc.uoc.pqtm.mybooks;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    /**
+     * Método llamado cuando se recibe un mensaje remoto
+     *
+     * @param remoteMessage Mensaje recibido de Firebase Cloud Messaging.
+     */
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Mostrar una notificación al recibir un mensaje de Firebase
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+        }
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            String body = remoteMessage.getNotification().getBody();
+            Log.d(TAG, "Message Notification Body: " + body);
+            //send notification
+            sendNotification(body);
+        }
+    }
+
+    /**
+     * Crea y muestra una notificación al recibir un mensaje de Firebase
+     *
+     * @param messageBody Texto a mostrar en la notificación
+     */
+    private void sendNotification(String messageBody) {
+
+        Intent intent = new Intent(this, BookListActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String channelId = getString(R.string.default_notification_channel_id);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, channelId)
+                        .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                        .setContentTitle(getString(R.string.fcm_message))
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
