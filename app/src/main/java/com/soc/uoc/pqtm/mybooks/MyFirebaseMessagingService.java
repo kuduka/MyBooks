151d65a2d5be5e04c7c31caaa306af17174791b4
kuduka <kuduka@gmail.com>
2018-10-28 10:55:06 +0100

    [ADD] Exer3.2

--- app/src/main/java/com/soc/uoc/pqtm/mybooks/MyFirebaseMessagingService.java
+++ app/src/main/java/com/soc/uoc/pqtm/mybooks/MyFirebaseMessagingService.java
@@ -13,9 +13,14 @@ import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Map;
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseMsgService";
+    private static final String BOOK_POSITION = "BOOK_POSITION";
+    private static final String ACTION_DELETE = "ACTION_DELETE";
+    private static final String ACTION_VIEW = "ACTION_VIEW";
 
     /**
      * Método llamado cuando se recibe un mensaje remoto
@@ -26,10 +31,13 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         // Mostrar una notificación al recibir un mensaje de Firebase
 
+        Map<String, String> data = null;
+
         Log.d(TAG, "From: " + remoteMessage.getFrom());
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            data = remoteMessage.getData();
+            Log.d(TAG, "Message data payload: " + data);
 
         }
         // Check if message contains a notification payload.
@@ -37,7 +45,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             String body = remoteMessage.getNotification().getBody();
             Log.d(TAG, "Message Notification Body: " + body);
             //send notification
-            sendNotification(body);
+            sendNotification(body, data);
         }
     }
 
@@ -46,13 +54,35 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      *
      * @param messageBody Texto a mostrar en la notificación
      */
-    private void sendNotification(String messageBody) {
+    private void sendNotification(String messageBody, Map<String, String>  messageData) {
 
-        Intent intent = new Intent(this, BookListActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
 
+        //parse book_position if it exists
+        String bookPos = messageData.get("book_position");
+        if (bookPos == null) {
+            Log.d(TAG, "Message Notification book_position not found");
+            return;
+        }
+
+        Log.d(TAG, "Message Notification book_position: " + bookPos);
+
+        //create 2 intents for viewing or deleting the book
+        Intent intentDelBook = new Intent(this, BookListActivity.class);
+        Intent intentViewBook = new Intent(this, BookListActivity.class);
+
+        intentDelBook.setAction(ACTION_DELETE);
+        intentDelBook.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        intentDelBook.putExtra(BOOK_POSITION, bookPos);
+        PendingIntent delBookIntent = PendingIntent.getActivity(this, 0,
+                intentDelBook, PendingIntent.FLAG_ONE_SHOT);
+
+        intentViewBook.setAction(ACTION_VIEW);
+        intentViewBook.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        intentViewBook.putExtra(BOOK_POSITION, bookPos);
+        PendingIntent viewBookIntent = PendingIntent.getActivity(this, 0,
+                intentViewBook, PendingIntent.FLAG_ONE_SHOT);
+
+        //create channel for notifications
         String channelId = getString(R.string.default_notification_channel_id);
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder =
@@ -62,7 +92,12 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                         .setContentText(messageBody)
                         .setAutoCancel(true)
                         .setSound(defaultSoundUri)
-                        .setContentIntent(pendingIntent);
+                        .addAction(new NotificationCompat.Action(
+                                R.drawable.ic_stat_ic_notification,
+                                "Delete book", delBookIntent))
+                        .addAction(new NotificationCompat.Action(
+                                R.drawable.ic_stat_ic_notification,
+                                "View book", viewBookIntent));
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
@@ -74,7 +109,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                     NotificationManager.IMPORTANCE_DEFAULT);
             notificationManager.createNotificationChannel(channel);
         }
-
+        //show notification
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 }
